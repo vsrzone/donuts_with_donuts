cc994e649ddce6327741654ac2dd872acaeb6401
vsrzone <vikumsri@gmail.com>
2014-09-24 19:05:57 +0530

    day1

--- index.php
+++ index.php
@@ -6,9 +6,56 @@
 
 <link rel="stylesheet" href="stylesheet.css" />
 
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="responsiveslides.min.js"></script>
+
+<script>
+  $(document).ready(function(e) {    
+	$(".rslides").responsiveSlides({
+	  auto: true,             // Boolean: Animate automatically, true or false
+	  speed: 500,            // Integer: Speed of the transition, in milliseconds
+	  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+	  pager: false,           // Boolean: Show pager, true or false
+	  nav: false,             // Boolean: Show navigation, true or false
+	  random: false,          // Boolean: Randomize the order of the slides, true or false
+	  pause: false,           // Boolean: Pause on hover, true or false
+	  pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+	  prevText: "Previous",   // String: Text for the "previous" button
+	  nextText: "Next",       // String: Text for the "next" button
+	  maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+	  navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+	  manualControls: "",     // Selector: Declare custom pager navigation
+	  namespace: "rslides",   // String: Change the default namespace used
+	  before: function(){},   // Function: Before callback
+	  after: function(){}     // Function: After callback
+	});
+  });
+
+
+
+</script>
+
+
+
+
 </head>
 
 <body>
+
+<div id="header">
+	
+    <div id="header_navi_back">
+        <div id="header_logo">
+        </div>
+	</div>
+    
+    <ul class="rslides">
+      <li><img src="images/slide-01.jpg" alt=""></li>
+      <li><img src="images/slide-02.jpg" alt=""></li>
+      <li><img src="images/slide-03.jpg" alt=""></li>	
+    </ul>
+    
+</div>
 
 <div id="container">
 </div>
